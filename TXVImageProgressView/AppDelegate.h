964022c88bc87617a18be57ad871ebf78426ee95
Tran Xuan Viet <viettranx@gmail.com>
2015-07-10 07:36:11 +0700

    Initial Commit

---
+++ TXVImageProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TXVImageProgressView
+//
+//  Created by Tran Viet on 7/10/15.
+//  Copyright (c) 2015 Viettranx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
